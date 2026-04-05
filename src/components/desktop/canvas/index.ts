95310073adf8507dcda4fd08fd03290b3e911d46
LavonTMCQ <lavontmcq@users.noreply.github.com>
2026-04-05 17:04:07 -0400

    refactor: decompose Canvas.tsx into modules (#499)

---
+++ src/components/desktop/canvas/index.ts
@@ -0,0 +1,5 @@
+export { CIViewer } from './CanvasCIViewer';
+export { MermaidViewer, DiffViewer } from './CanvasDiffMermaidViewers';
+export { DeployViewer, GitLogViewer, ImagePreview, ReadmeViewer } from './CanvasResourceViewers';
+export { CanvasEmpty, PortPreview, TranscriptViewer } from './TranscriptCanvasViewers';
+export { TimelineExpanded } from './TimelineExpanded';
